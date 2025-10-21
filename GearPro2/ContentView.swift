6892f9dddb3f5d0c2526953a57d1bcbf13b48a92
anorville88 <austin.norville@gmail.com>
2025-10-21 18:26:52 -0500

    Initial Commit

---
+++ GearPro2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GearPro2
+//
+//  Created by Austin Norville on 10/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GearPro2/GearPro2App.swift
@@ -0,0 +1,17 @@
+//
+//  GearPro2App.swift
+//  GearPro2
+//
+//  Created by Austin Norville on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GearPro2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
